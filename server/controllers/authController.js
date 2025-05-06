d348e4957073bbcdb64b745d9b16f0389082f4d6
Henry1610 <trannguyentruong6@gmail.com>
2025-05-06 16:44:19 +0700

    update login register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,36 +2,40 @@ const User = require('../models/user')
 const bcrypt = require('bcryptjs');
 const generateToken = require('../utils/generateToken');
 
-exports.login=async function (req,res) {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    // if (!user || !(await bcrypt.compare(password, user.password))) {
-    //     return res.status(401).json({ message: 'Invalid credentials' });
-    // }
-    if (!user) {
-        return res.status(401).json({ message: 'User not found' });
-    }
-    
-    const isValidPassword = await user.isPasswordValid(password);
+exports.login = async function (req, res) {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        // if (!user || !(await bcrypt.compare(password, user.password))) {
+        //     return res.status(401).json({ message: 'Invalid credentials' });
+        // }
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        const isValidPassword = await user.isPasswordValid(password);
         if (!isValidPassword) {
             return res.status(401).json({ message: 'Mật khẩu không đúng' });
         }
-    
-    const token = generateToken(user._id, user.role);
-    res.json({ token });
+
+        const token = generateToken(user._id, user.role);
+        res.json({ token });
+    } catch (error) {
+        console.error('Register error:', error);
+        res.status(500).json({ message: 'Lỗi server', error: error.message });
+    }
 }
-exports.register=async function (req,res) {
-    try{
-        const {username,email,password}=req.body
-        const existingUser = await User.findOne({  email  });
-        if(existingUser)
-        {
+exports.register = async function (req, res) {
+    try {
+        const { username, email, password } = req.body
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
             return res.status(400).json({ message: 'Username hoặc email đã tồn tại' });
-    
+
         }
-        
-        const user=await User.create({
-            
+
+        const user = await User.create({
+
             username,
             email,
             password
@@ -41,16 +45,16 @@ exports.register=async function (req,res) {
         res.status(201).json({
             message: 'Đăng ký thành công',
             user: {
-              id: user._id,
-              username: user.username,
-              email: user.email,
+                id: user._id,
+                username: user.username,
+                email: user.email,
             },
             token,
-          });
-    
-    }catch{
+        });
+
+    } catch (error) {
         res.status(500).json({ message: 'Lỗi server', error: error.message });
 
     }
-    
+
 }
